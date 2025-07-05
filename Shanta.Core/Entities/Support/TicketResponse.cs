45aa220e8d273d8ff3d1c67c38ecdc7a52860cfb
Ahmed Ibrahim <ai.ahmed.ibrahim21@gmail.com>
2025-07-05 17:48:22 +0300

    ➕Support Entities (Ticket,Status,TicketResponse)

---
+++ Shanta.Core/Entities/Support/TicketResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shanta.Core.Entities.Support
+{
+    public class TicketResponse : BaseEntity
+    {
+        public string? ResponseText { get; set; } // The text of the response
+        public DateTime CreatedAt { get; set; } // Date and time when the response was created
+        public string? UserId { get; set; } // ID of the user who provided the response
+        public string TicketId { get; set; } // ID of the associated support ticket
+        public SupportTicket? Ticket { get; set; } // Navigation property to the associated support ticket
+    }
+}
