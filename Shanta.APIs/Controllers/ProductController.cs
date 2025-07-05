5af418a045e47ffbda2154045a840bd9caa340b4
Ahmed 20210063 <Ahmed_20210063@fci.helwan.edu.eg>
2025-07-05 04:19:44 +0300

    ➕ Creating ProductController

---
+++ Shanta.APIs/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shanta.Core.Entities.Product;
+using Shanta.Core.Repository.Contract;
+
+namespace Shanta.APIs.Controllers
+{
+
+    public class ProductController : BaseApiController
+    {
+        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IGenericRepository<ProductBrand> _brandRepository;
+        private readonly IGenericRepository<ProductCategory> _categoryRepository;
+
+        public ProductController
+            (
+                IGenericRepository<Product> productRepository,
+                IGenericRepository<ProductBrand> brandRepository,
+                IGenericRepository<ProductCategory> categoryRepository
+            )
+        {
+            _productRepository = productRepository;
+            _brandRepository = brandRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+
+    }
+}
