18e137a1d0f122ad69ad8be36e5ddac41a7382af
Ahmed 20210063 <Ahmed_20210063@fci.helwan.edu.eg>
2025-07-07 02:40:07 +0300

    ➕ Creating ProductDTO

---
+++ Shanta.APIs/DTOs/ProductDTO.cs
@@ -0,0 +1,34 @@
+﻿using Shanta.Core.Entities.Product;
+
+namespace Shanta.APIs.DTOs
+{
+    public class ProductDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public int? Rating { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Guid BrandId { get; set; } 
+
+        public string Brand { get; set; } 
+
+        public Guid CategoryId { get; set; } 
+        public string Category { get; set; } 
+
+        public bool HasVariants { get; set; } 
+
+        public List<string> Variants { get; set; } 
+
+        public bool OutOfStock { get; set; } 
+
+        public int TotalStock { get; set; }
+    }
+}
