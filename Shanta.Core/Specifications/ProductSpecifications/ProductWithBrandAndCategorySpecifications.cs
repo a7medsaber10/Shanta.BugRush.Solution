f30cf5f0e21770a9788c7c1790f976b9e25c0350
Ahmed 20210063 <Ahmed_20210063@fci.helwan.edu.eg>
2025-07-07 02:37:06 +0300

    ➕Adding Product With Brand And Category Specifications (Includes)

---
+++ Shanta.Core/Specifications/ProductSpecifications/ProductWithBrandAndCategorySpecifications.cs
@@ -0,0 +1,24 @@
+﻿using Shanta.Core.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shanta.Core.Specifications.ProductSpecifications
+{
+    public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
+    {
+        public ProductWithBrandAndCategorySpecifications()
+        {
+            Includes.Add(p => p.Brand);
+            Includes.Add(p => p.Category);
+        }
+
+        public ProductWithBrandAndCategorySpecifications(Guid id) : base(p => p.Id == id)
+        {
+            Includes.Add(p => p.Brand);
+            Includes.Add(p => p.Category);
+        }
+    }
+}
