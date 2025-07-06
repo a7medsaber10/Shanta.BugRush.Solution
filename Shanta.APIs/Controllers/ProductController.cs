d3ff463cf3229277fc49e26496411191865355a5
Ahmed 20210063 <Ahmed_20210063@fci.helwan.edu.eg>
2025-07-07 02:43:29 +0300

    ➕ Adding GetProducts Endpoint (version 1 - with includes only)

--- Shanta.APIs/Controllers/ProductController.cs
+++ Shanta.APIs/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Shanta.APIs.DTOs;
 using Shanta.Core.Entities.Product;
 using Shanta.Core.Repository.Contract;
+using Shanta.Core.Specifications.ProductSpecifications;
 
 namespace Shanta.APIs.Controllers
 {
@@ -10,17 +13,31 @@ namespace Shanta.APIs.Controllers
         private readonly IGenericRepository<Product> _productRepository;
         private readonly IGenericRepository<ProductBrand> _brandRepository;
         private readonly IGenericRepository<ProductCategory> _categoryRepository;
+        private readonly IMapper _mapper;
 
         public ProductController
             (
                 IGenericRepository<Product> productRepository,
                 IGenericRepository<ProductBrand> brandRepository,
-                IGenericRepository<ProductCategory> categoryRepository
+                IGenericRepository<ProductCategory> categoryRepository,
+                IMapper mapper
             )
         {
             _productRepository = productRepository;
             _brandRepository = brandRepository;
             _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("all-products")]
+        public async Task<ActionResult<IReadOnlyList<ProductDTO>>> GetProducts()
+        {
+            var spec = new ProductWithBrandAndCategorySpecifications();
+            var products = await _productRepository.GetAllWithSpecAsync(spec);
+
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductDTO>>(products);
+
+            return Ok(data);
         }
 
 
